18929740a5955d9917aa28809a265e64f3b2ab37
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-11-28 18:55:27 +0300

    Moved exceptions to namespace

--- src/include/exceptions.h
+++ src/include/exceptions.h
@@ -1,18 +1,18 @@
 #ifndef MERGEABLEHEAPS_EXCEPTIONS_H
 #define MERGEABLEHEAPS_EXCEPTIONS_H
 
-class WrongHeapTypeException : public std::exception {
-    [[nodiscard]] const char* what () const noexcept override
-    {
-        return "Can't merge different types of heaps";
-    }
-};
+namespace heaps {
+    class WrongHeapTypeException : public std::exception {
+        [[nodiscard]] const char *what() const noexcept override {
+            return "Can't merge different types of heaps";
+        }
+    };
 
-class EmptyHeapException : public std::exception {
-    [[nodiscard]] const char* what () const noexcept override
-    {
-        return "Can't access an element of an empty heap";
-    }
-};
+    class EmptyHeapException : public std::exception {
+        [[nodiscard]] const char *what() const noexcept override {
+            return "Can't access an element of an empty heap";
+        }
+    };
+}
 
 #endif //MERGEABLEHEAPS_EXCEPTIONS_H
