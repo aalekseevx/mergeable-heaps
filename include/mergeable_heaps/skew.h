d4ce0c00a02e0dc81f01be63bbf8d9fe3ea7c29c
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-12-04 21:49:32 +0300

    Implemented skew heap using classical_heap

--- include/mergeable_heaps/skew.h
+++ include/mergeable_heaps/skew.h
@@ -1,166 +1,17 @@
-#ifndef MERGEABLEHEAPS_SKEW_H
-#define MERGEABLEHEAPS_SKEW_H
+#ifndef MERGEABLE_HEAPS_SKEW_H
+#define MERGEABLE_HEAPS_SKEW_H
 
 #include "iheap.h"
 #include "exceptions.h"
 #include "nodes/skew_heap_node.h"
+#include "classical_heap.h"
 
 namespace heaps {
     template<class Key = int>
-    class SkewHeap : public IHeap<Key> {
-        SkewHeapNode<Key> *root;
-        size_t size;
-
-        void Merge_(SkewHeap &x);
-
+    class SkewHeap : public ClassicalHeap<Key, SkewHeapNode<Key>> {
     public:
-        SkewHeap();
-
-        explicit SkewHeap(Key key);
-
-        void Insert(Key x) override;
-
-        Key GetMinimum() override;
-
-        void ExtractMinimum() override;
-
-        void Merge(IHeap <Key> &x) override;
-
-        size_t Size() override;
-
-        bool Empty() override;
-
-        void Detach() override;
-
-        // Rule of Five functions
-        ~SkewHeap<Key>();
-
-        SkewHeap<Key>(const SkewHeap<Key> &other);
-
-        SkewHeap<Key>(SkewHeap<Key> &&other) noexcept;
-
-        SkewHeap<Key> &operator=(const SkewHeap<Key> &other);
-
-        SkewHeap<Key> &operator=(SkewHeap<Key> &&other) noexcept;
-
-        void Swap(SkewHeap<Key> &x) noexcept;
+        using ClassicalHeap<Key, SkewHeapNode<Key>>::ClassicalHeap;
     };
-
-    template<class Key>
-    void SkewHeap<Key>::Insert(Key x) {
-        Merge(*new SkewHeap(x));
-    }
-
-    template<class Key>
-    Key SkewHeap<Key>::GetMinimum() {
-        if (Empty()) {
-            throw EmptyHeapException();
-        } else {
-            return root->key;
-        }
-    }
-
-    template<class Key>
-    void SkewHeap<Key>::ExtractMinimum() {
-        if (Empty()) {
-            throw EmptyHeapException();
-        } else {
-            SkewHeapNode<Key> *left = root->child_left;
-            SkewHeapNode<Key> *right = root->child_right;
-            root->Detach();
-            delete root;
-            root = SkewHeapNode<Key>::Merge_(left, right);
-        }
-    }
-
-    template<class Key>
-    void SkewHeap<Key>::Merge(IHeap <Key> &x) {
-        if (&x == this) {
-            throw SelfHeapMergeException();
-        }
-        try {
-            Merge_(dynamic_cast<SkewHeap<Key> &>(x));
-            x.Detach();
-        } catch (const std::bad_cast &e) {
-            throw WrongHeapTypeException();
-        }
-    }
-
-    template<class Key>
-    SkewHeap<Key>::SkewHeap(Key key) : size(1) {
-        root = new SkewHeapNode<Key>(key, nullptr, nullptr);
-    }
-
-    template<class Key>
-    size_t SkewHeap<Key>::Size() {
-        return Empty() ? 0 : size;
-    }
-
-    template<class Key>
-    bool SkewHeap<Key>::Empty() {
-        return root == nullptr;
-    }
-
-    template<class Key>
-    void SkewHeap<Key>::Merge_(SkewHeap &x) {
-        root = SkewHeapNode<Key>::Merge_(root, x.root);
-    }
-
-    template<class Key>
-    SkewHeap<Key>::SkewHeap() : root(nullptr), size(0) {}
-
-    template<class Key>
-    void SkewHeap<Key>::Detach() {
-        root = nullptr;
-        size = 0;
-    }
-
-    // Destructor
-    template<class Key>
-    SkewHeap<Key>::~SkewHeap<Key>() {
-        if (root != nullptr) {
-            delete root;
-        }
-    }
-
-    // Copy constructor
-    template<class Key>
-    SkewHeap<Key>::SkewHeap(const SkewHeap<Key> &other) : size(other.size), root(new SkewHeapNode(*other.root)) {}
-
-    // Move constructor
-    template<class Key>
-    SkewHeap<Key>::SkewHeap(SkewHeap<Key> &&other) noexcept {
-        root = nullptr;
-        size = 0;
-        Swap(other);
-    }
-
-    // Copy assignment operator
-    template<class Key>
-    SkewHeap<Key> &SkewHeap<Key>::operator=(const SkewHeap<Key> &other) {
-        if (this != &other) {
-            SkewHeap tmp(other);
-            Swap(tmp);
-        }
-        return *this;
-    }
-
-    // Move assignment operator
-    template<class Key>
-    SkewHeap<Key> &SkewHeap<Key>::operator=(SkewHeap<Key> &&other) noexcept {
-        if (this != &other) {
-            root = nullptr;
-            size = 0;
-            Swap(other);
-        }
-        return *this;
-    }
-
-    template<class Key>
-    void SkewHeap<Key>::Swap(SkewHeap<Key> &x) noexcept {
-        std::swap(root, x.root);
-        std::swap(size, x.size);
-    }
 }
 
-#endif //MERGEABLEHEAPS_SKEW_H
+#endif //MERGEABLE_HEAPS_SKEW_H
--- src/include/classical_heap.h
+++ src/include/classical_heap.h
@@ -1,4 +1,172 @@
 #ifndef MERGEABLE_HEAPS_CLASSICAL_HEAP_H
 #define MERGEABLE_HEAPS_CLASSICAL_HEAP_H
 
+#include "mergeable_heaps/exceptions.h"
+#include "iheap.h"
+#include "nodes/classical_heap_node.h"
+
+namespace heaps {
+    template<class Key, class NodeType>
+    class ClassicalHeap : public IHeap<Key> {
+    private:
+        NodeType *root;
+        
+        size_t size;
+
+        void Merge_(ClassicalHeap &x);
+
+    public:
+        ClassicalHeap();
+
+        explicit ClassicalHeap(Key key);
+
+        void Insert(Key x) override;
+
+        Key GetMinimum() override;
+
+        void ExtractMinimum() override;
+
+        void Merge(IHeap<Key> &x) override;
+
+        size_t Size() override;
+
+        bool Empty() override;
+
+        void Detach() override;
+
+        //
+        // Rule of Five functions
+        //
+        
+        ~ClassicalHeap<Key, NodeType>();
+
+        ClassicalHeap<Key, NodeType>(const ClassicalHeap<Key, NodeType> &other);
+
+        ClassicalHeap<Key, NodeType>(ClassicalHeap<Key, NodeType> &&other) noexcept;
+
+        ClassicalHeap<Key, NodeType> &operator=(const ClassicalHeap<Key, NodeType> &other);
+
+        ClassicalHeap<Key, NodeType> &operator=(ClassicalHeap<Key, NodeType> &&other) noexcept;
+
+        void Swap(ClassicalHeap<Key, NodeType> &x) noexcept;
+    };
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::Insert(Key x) {
+        Merge(*new ClassicalHeap(x));
+    }
+
+    template<class Key, class NodeType>
+    Key ClassicalHeap<Key, NodeType>::GetMinimum() {
+        if (Empty()) {
+            throw EmptyHeapException();
+        } else {
+            return root->key;
+        }
+    }
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::ExtractMinimum() {
+        if (Empty()) {
+            throw EmptyHeapException();
+        } else {
+            NodeType *left = root->child_left;
+            NodeType *right = root->child_right;
+            root->Detach();
+            delete root;
+            root = NodeType::Merge_(left, right);
+        }
+    }
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::Merge(IHeap<Key> &x) {
+        if (&x == this) {
+            throw SelfHeapMergeException();
+        }
+        try {
+            Merge_(dynamic_cast<ClassicalHeap<Key, NodeType> &>(x));
+            x.Detach();
+        } catch (const std::bad_cast &e) {
+            throw WrongHeapTypeException();
+        }
+    }
+
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType>::ClassicalHeap(Key key) : size(1) {
+        root = new NodeType(key, nullptr, nullptr);
+    }
+
+    template<class Key, class NodeType>
+    size_t ClassicalHeap<Key, NodeType>::Size() {
+        return Empty() ? 0 : size;
+    }
+
+    template<class Key, class NodeType>
+    bool ClassicalHeap<Key, NodeType>::Empty() {
+        return root == nullptr;
+    }
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::Merge_(ClassicalHeap &x) {
+        root = NodeType::Merge_(root, x.root);
+    }
+
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType>::ClassicalHeap() : root(nullptr), size(0) {}
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::Detach() {
+        root = nullptr;
+        size = 0;
+    }
+
+    // Destructor
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType>::~ClassicalHeap<Key, NodeType>() {
+        if (root != nullptr) {
+            delete root;
+        }
+    }
+
+    // Copy constructor
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType>::ClassicalHeap(const ClassicalHeap<Key, NodeType> &other) : size(other.size),
+                                                                         root(new ClassicalHeapNode(*other.root)) {}
+
+    // Move constructor
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType>::ClassicalHeap(ClassicalHeap<Key, NodeType> &&other) noexcept {
+        root = nullptr;
+        size = 0;
+        Swap(other);
+    }
+
+    // Copy assignment operator
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType> &ClassicalHeap<Key, NodeType>::operator=(const ClassicalHeap<Key, NodeType> &other) {
+        if (this != &other) {
+            ClassicalHeap tmp(other);
+            Swap(tmp);
+        }
+        return *this;
+    }
+
+    // Move assignment operator
+    template<class Key, class NodeType>
+    ClassicalHeap<Key, NodeType> &ClassicalHeap<Key, NodeType>::operator=(ClassicalHeap<Key, NodeType> &&other) noexcept {
+        if (this != &other) {
+            root = nullptr;
+            size = 0;
+            Swap(other);
+        }
+        return *this;
+    }
+
+    template<class Key, class NodeType>
+    void ClassicalHeap<Key, NodeType>::Swap(ClassicalHeap<Key, NodeType> &x) noexcept {
+        std::swap(root, x.root);
+        std::swap(size, x.size);
+    }
+}
+
 #endif //MERGEABLE_HEAPS_CLASSICAL_HEAP_H
